b9328b56e258ab48d703f9ecf113df266aab85d3
ST0817 <otogeya2032@gmail.com>
2025-07-23 15:04:46 +0900

    repl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use rustyline::{DefaultEditor, Result, error::ReadlineError};
+
+fn main() -> Result<()> {
+    let mut editor = DefaultEditor::new()?;
+    loop {
+        let read = editor.readline(">> ");
+        match read {
+            Ok(line) => {
+                editor.add_history_entry(&line)?;
+                println!(" => {}", line);
+            }
+            Err(ReadlineError::Interrupted) => {
+                println!("Interrupted");
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                println!("Quit");
+                break;
+            }
+            Err(err) => {
+                eprintln!("Error: {:?}", err);
+                break;
+            }
+        }
+    }
+    Ok(())
 }
