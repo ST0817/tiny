8ce03f76850bfbb626c151e4dec38894d5286ba9
ST0817 <otogeya2032@gmail.com>
2025-07-29 17:47:58 +0900

    REPL

--- src/Main.purs
+++ src/Main.purs
@@ -4,8 +4,17 @@ import Prelude
 
 import Effect (Effect)
 import Effect.Console (log)
+import Node.EventEmitter (on_)
+import Node.ReadLine (close, createConsoleInterface, lineH, noCompletion, prompt, setPrompt)
 
 main :: Effect Unit
 main = do
-  log "🍝"
-
+  interface <- createConsoleInterface noCompletion
+  setPrompt ">> " interface
+  prompt interface
+  interface # on_ lineH case _ of
+    "" -> prompt interface
+    ":quit" -> close interface
+    input -> do
+      log input
+      prompt interface
